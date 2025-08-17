429dfafea1d9abc78e9886ee5cc9e25cf323c321
gabemods <gabriel.longshaw@gmail.com>
2025-08-17 17:21:59 +0100

    feat (assets/js/hover-on-touch-screen.js): fixed problems with hover

--- assets/js/hover-on-touch-screen.js
+++ assets/js/hover-on-touch-screen.js
@@ -1,11 +1,11 @@
 document.querySelectorAll(".start-screen-button, .restart-btn, #leaveGameBtn, .back-button").forEach(button => {
-  button.addEventListener("touchstart", () => {
+  button.addEventListener("touchstart", (e) => {
     button.classList.add("hover");
-  });
-  button.addEventListener("touchend", () => {
-    button.classList.remove("hover");
-  });
-  button.addEventListener("touchcancel", () => {
-    button.classList.remove("hover");
-  });
+  }, { passive: true });
+  
+  const removeHover = () => button.classList.remove("hover");
+  
+  button.addEventListener("touchend", removeHover, { passive: true });
+  button.addEventListener("touchcancel", removeHover, { passive: true });
+  button.addEventListener("touchmove", removeHover, { passive: true });
 });
