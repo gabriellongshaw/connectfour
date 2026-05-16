41dd367ad7f75cd9dc74ba8ec154545dbd125d9a
gabriellongshaw <gabriel.longshaw@gmail.com>
2026-05-16 11:15:35 +0100

    fix(assets/js/core/firebase.js): remove beforeunload anonymous user deletion

--- assets/js/core/firebase.js
+++ assets/js/core/firebase.js
@@ -15,11 +15,6 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 
-window.addEventListener('beforeunload', () => {
-  const user = auth.currentUser;
-  if (user && user.isAnonymous) user.delete();
-});
-
 export function waitForAuth() {
   return new Promise((resolve, reject) => {
     const unsub = onAuthStateChanged(auth, user => {
